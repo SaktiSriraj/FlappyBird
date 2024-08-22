e254298ae536501709ea3614d364337b6910c28d
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2024-08-22 10:53:21 +0530

    Set Background and Initialize Images

---
+++ src/FlappyBird.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel {
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    // Images
+    Image backgroundImage;
+    Image birdImage;
+    Image topPipeImage;
+    Image bottomPipeImage;
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLUE);
+
+        // Load Images
+        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+}
